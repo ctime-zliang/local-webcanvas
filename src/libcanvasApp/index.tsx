7a0eca9926403f6f4886fced2095fe10ba9dbcdf
ctime-zliang <1156282805@qq.com>
2022-10-19 17:33:53 +0800

    feat: modify - daily commit

--- src/libcanvasApp/index.tsx
+++ src/libcanvasApp/index.tsx
@@ -1,15 +1,14 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOMClient from 'react-dom/client'
 import I18nProvider from './i18n/I18nProvider'
 import App from './App'
 
 export function renderReactApp(): void {
 	const __render_id__: number = Math.random()
-	ReactDOM.render(
+	ReactDOMClient.createRoot(document.getElementById('reactApp') as HTMLElement).render(
 		<I18nProvider>
 			<App __RenderProps__={{ __render_id__ }} />
-		</I18nProvider>,
-		document.getElementById('reactApp')
+		</I18nProvider>
 	)
 }
 
